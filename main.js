c22c3df3b6b0a5c3660cb41957fdb03144fdefc0
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-08-16 16:12:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const API_KEY = "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";
 
-const discussionBody = "What is Github?"; //process.env.DISCUSSION_BODY;
+const discussionBody = "\"What is Github?\""; //process.env.DISCUSSION_BODY;
 
 const generate = async () => {
   try {
